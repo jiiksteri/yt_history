64550bb1d90f1607eaa857badace1b874e7dd953
Jukka Ollila <jukka.ollila@iki.fi>
2012-12-17 18:31:55 +0200

    list: Hook the feed parser into list_handle()
    Oh woo, managed to come up with an interface to the parser
    that only requires flipping argument order in the callback.
    
    Yay me.
    
    There's a lot of duplicates in the list too, wonder what to
    do with those.
    
    Perhaps we should grab videoid and do uniqueness checks based on the
    video id or something, updating F_PUBLISHED/F_UPDATED accordingly. But
    this would mean not being able to spew entries at the browser the minute
    they appear.
    
    Not that they appear immediately anyway, as we're kind of doing this in
    the thread trying to handle the request...

--- list.c
+++ list.c
@@ -4,18 +4,15 @@
 #include "store.h"
 #include "auth.h"
 #include "https.h"
+#include "feed.h"
 
 #include <stdio.h>
+#include <string.h>
 
 static void read_list(struct evbuffer *buf, void *arg)
 {
-	char data[1024];
-	int removed;
-
-	while ((removed = evbuffer_remove(buf, data, sizeof(data)-1)) > 0) {
-		data[removed] = '\0';
-		printf("%s(): %s", __func__, data);
-	}
+	struct feed *feed = arg;
+	feed_consume(feed, buf);
 }
 
 void list_handle(struct auth_engine *auth, struct session *session,
@@ -23,6 +20,8 @@ void list_handle(struct auth_engine *auth, struct session *session,
 {
 	char *err_msg;
 	const char *access_token;
+	struct feed *feed;
+	int err;
 
 	access_token = session_get_value(session, "access_token");
 	printf("%s(): using access token %s\n", __func__,
@@ -36,23 +35,28 @@ void list_handle(struct auth_engine *auth, struct session *session,
 	}
 
 
+	if ((err = feed_init(&feed, evhttp_request_get_output_buffer(req))) != 0) {
+		printf("%s(): feed_init(): %s\n", __func__, strerror(err));
+		evhttp_send_error(req, HTTP_INTERNAL, "feed_init() failed");
+	}
+
+
 	err_msg = https_request(auth_https(auth),
 				"gdata.youtube.com", 443,
 				"GET",
 				"/feeds/api/users/default/watch_history?v=2",
 				access_token,
 				(struct evbuffer *)NULL,
-				read_list, NULL);
+				read_list, feed);
+
+	feed_final(feed);
+	feed_destroy(feed);
 
 	if (err_msg != NULL) {
 		evhttp_send_error(req, HTTP_INTERNAL, err_msg);
 	} else {
-		/* Either we've spewed the data directly from
-		 * read_list() or we should provide a context
-		 * argument for it and do it here. Think.
-		 */
-		evhttp_send_error(req, HTTP_INTERNAL,
-				  "Someone needs to think");
+		evhttp_send_reply(req, HTTP_OK, "OK",
+				  evhttp_request_get_output_buffer(req));
 	}
 	free(err_msg);
 }
